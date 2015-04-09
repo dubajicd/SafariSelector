7b567de79a965c772122b0bb2e96329789343a40
dubajicd <dubajicd@gmail.com>
2015-04-09 11:13:50 +0200

    Create safariselector.js

---
+++ safariselector.js
@@ -0,0 +1,9 @@
+    // Vertical alignement for Safari 4 and 5
+    var checkOldSafari = String(navigator.userAgent.match(/Version\/[\d\.]+.*Safari/));
+    var res = String(checkOldSafari.match(/[\d\.]+/));
+    var newres = parseInt(res.substring(0, 1));
+    if(newres < 6){
+      /**
+      * Enter code here
+      */
+    }
